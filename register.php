a559150fdc6201cbb99a1f7e3c23f39867e7f26b
Bisferoz45 <gabriel.cardenas.martin@gmail.com>
2025-10-08 19:09:34 +0100

    V6.0
    Suggestions para la contraseña (Falta añadir validación)

--- register.php
+++ register.php
@@ -9,6 +9,23 @@ $_SESSION["logged"] = false;
 <html>
     <head>
         <title>AlmAnime</title>
+        <script>
+            function showHint(str) {
+                if (str.length == 0) {
+                document.getElementById("txtHint").innerHTML = "";
+                return;
+                }else{
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("txtHint").innerHTML = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET", "gethint.php?q=" + str, true);
+                    xmlhttp.send();
+                }
+            }
+        </script>
     </head>
     <body>
             <h1>Registrarse</h1>
@@ -17,7 +34,9 @@ $_SESSION["logged"] = false;
         <form action="validacion.php" method="POST">
             Usuario: <input type="text" name="user" placeholder="Username" value="<?php echo $user?>"> <br>
             E-mail: <input type="email" name="email" placeholder="example@domain.es" value="<?php echo $email?>"> <br>
-            Contraseña: <input type="password" name="passwd1" placeholder="Password"> <br>
+            Contraseña: <input type="password" name="passwd1" placeholder="Password" onkeyup="showHint(this.value)"> <br>
+            Suggestions: <span id="txtHint"></span>
+            
             Confirmar contraseña: <input type="password" name="passwd2" placeholder="Confirm password"> <br>
             <input type="submit" name="submit"> <br> <br>
             <h3>Ya tiene una cuenta?:<h3> <a href="loggin.php"><input type="button" value="Logearse"></a>
