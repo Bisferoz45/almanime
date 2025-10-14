8beed593ee570cf250d64af8ee0940d6511637ce
Bisferoz45 <gabriel.cardenas.martin@gmail.com>
2025-10-14 16:34:37 +0100

    V8.0
    Añadida barra de búsqueda, se deve reestructurar tanto la propia página como la base de datos

--- MainPage/aniSearch.php
+++ MainPage/aniSearch.php
@@ -5,15 +5,16 @@ require "../conect.php";
 // get the q parameter from URL
 $q = clearSentence( $_REQUEST["q"]);
 
-echo $q . "<br>";
-
 if($q !== ""){
-    $res = $conn->prepare("SELECT titulo, description, demo, img FROM almanime.animes WHERE titulo LIKE ?");
-    echo print_r($res);
-    $res->execute([$q]);
-    $res->fetchAll(PDO::FETCH_ASSOC);
-    echo print_r($res);
-
+    $rows = $conn->prepare("SELECT * FROM almanime.animes WHERE titulo LIKE ?");
+    $rows->execute(["$q%"]);
+    $rows = $rows->fetchAll(PDO::FETCH_ASSOC);
+    if($rows){
+        foreach($rows as $row){ 
+            echo '<a href="aniShow.php?id=' . urldecode($row["titulo"]) .'"><img class="imgPort" height="200px" width="300px" src="' . htmlspecialchars($row["img"]) . '"></a>';
+            echo '<p>Tírulo: ' . htmlspecialchars($row["titulo"]) . "<br> Demografía: " . htmlspecialchars($row["demo"]) . "<br> Descripción:<br>" . htmlspecialchars($row["description"]) . '</p>';
+        }
+    }
 }else{
     $res = $conn->query("SELECT titulo, description, demo, img FROM almanime.animes");
     $rows = $res->fetchAll(PDO::FETCH_ASSOC);
@@ -22,9 +23,10 @@ if($q !== ""){
             echo '<a href="aniShow.php?id=' . urldecode($row["titulo"]) .'"><img class="imgPort" height="200px" width="300px" src="' . htmlspecialchars($row["img"]) . '"></a>';
             echo '<p>Tírulo: ' . htmlspecialchars($row["titulo"]) . "<br> Demografía: " . htmlspecialchars($row["demo"]) . "<br> Descripción:<br>" . htmlspecialchars($row["description"]) . '</p>';
         }
+    }else{
+        echo 'No hay animes para mostrar de momento.';
     }
 }
-
 
 function clearSentence($data) {
   $data = trim($data);
--- MainPage/index.php
+++ MainPage/index.php
@@ -16,8 +16,14 @@ $_SESSION["error"] = '';
         <script>
             function serchAnime(str) {
                 if (str.length == 0) {
-                    //document.getElementById("resAniSearch").innerHTML = "";
-                    return;
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("resAniSearch").innerHTML = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET", "aniSearch.php?q=" + str, true);
+                    xmlhttp.send();
                 }else{
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function() {
@@ -28,10 +34,6 @@ $_SESSION["error"] = '';
                     xmlhttp.open("GET", "aniSearch.php?q=" + str, true);
                     xmlhttp.send();
                 }
-            }
-
-            function showAnimes(){
-                
             }
         </script>
     </head>
@@ -50,16 +52,22 @@ $_SESSION["error"] = '';
             BUSCADOR: <input type="serch" name="aniSerch" placeholder="Busca un anime" onkeyup="serchAnime(this.value)"><br>
             <div class="aniCntnr" id="resAniSearch">
                 <?php
-                    $res = $conn->query("SELECT titulo, description, demo, img FROM almanime.animes");
-                    $rows = $res->fetchAll(PDO::FETCH_ASSOC);
+                    showAnimesIndex();
 
-                    if($rows){
-                        foreach($rows as $row){
-                            echo '<a href="aniShow.php?id=' . urldecode($row["titulo"]) .'"><img class="imgPort" height="200px" width="300px" src="' . htmlspecialchars($row["img"]) . '"></a>';
-                            echo '<p>Tírulo: ' . htmlspecialchars($row["titulo"]) . "<br> Demografía: " . htmlspecialchars($row["demo"]) . "<br> Descripción:<br>" . htmlspecialchars($row["description"]) . '</p>';
+                    function showAnimesIndex(){
+                        require "../conect.php";
+                        $res = $conn->query("SELECT titulo, description, demo, img FROM almanime.animes");
+                        $rows = $res->fetchAll(PDO::FETCH_ASSOC);
+                        if($rows){
+                            foreach($rows as $row){
+                                echo '<div id="anime">';
+                                echo '<a href="aniShow.php?id=' . urldecode($row["titulo"]) .'"><img class="imgPort" height="200px" width="300px" src="' . htmlspecialchars($row["img"]) . '"></a>';
+                                echo '<p>Tírulo: ' . htmlspecialchars($row["titulo"]) . "<br> Demografía: " . htmlspecialchars($row["demo"]) . "<br> Descripción:<br>" . htmlspecialchars($row["description"]) . '</p>';
+                                echo '</div>';
+                            }
+                        }else{
+                            echo 'No hay animes para mostrar de momento.';
                         }
-                    }else{
-                        echo 'No hay animes para mostrar de momento.';
                     }
                 ?>
             </div>
